00bc9d737836132521e42841962ebe8c027403a7
Arif K Manasiya <arifmanasiya@gmail.com>
2025-12-09 14:58:48 -0600

    Remove topbar title/content; keep empty header

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,12 +1,3 @@
 export default function TopNav() {
-  return (
-    <header className="topnav">
-      <div className="title">
-        <span role="img" aria-label="shield">
-          ⛽️
-        </span>
-        FuelSense Demo
-      </div>
-    </header>
-  );
+  return <header className="topnav" />;
 }
